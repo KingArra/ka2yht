6ee2de42d4ab1a763ae28021537da24ab7c7bcde
KingArra <67718971+KingArra@users.noreply.github.com>
2020-09-01 12:10:07 -0400

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -3973,7 +3973,7 @@
           var c = [
             [
               "Tip: You can view and edit your keybinds in the options menu.",
-              "Tip: You can play on mobile by just going to arras.io on your phone!"
+              "Tip: You can play on mobile by just going to arras-ka2.netlify.app on your phone!"
             ],
             [
               "Tip: You can have the shield and health bar be separated by going to the options menu.",
@@ -3983,9 +3983,14 @@
               "Tip: You can create your own theme with the custom theme makerin the link on the options menu."
             ],
             [
-              "Teaming in FFA or FFA Maze is frowned upon, but when taken to the extremes, you can be punished.",
+              "If your a Beta Tester, press ' to be a Developer.",
               "Witch hunting is when you continuously target someone and follow them. This is frowned upon, but when taken to the extremes, you can be punished.",
               "Multiboxing is when you use a script to control multiple tanks at the same time. This is considered CHEATING and will result in a ban."
+                      ],
+            [
+              "Join the KA2 Arras Discord!",
+              "Didn't Subscribe to King Arras? Go to youtube.com/kingarras now!",
+              "Police Boosters are allowed to kill people who abuse the game!"
             ]
           ];
           c = c[Math.floor(Math.random() * c.length)];
